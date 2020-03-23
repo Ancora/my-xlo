7fea8f1f5e4e95c68ef599a56c94fa591e0e738d
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-23 15:27:49 -0300

    301-36-Adic. Anúncios na Tela Principal.

--- lib/screens/create/create_screen.dart
+++ lib/screens/create/create_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:myxlo/blocs/create_bloc.dart';
 import 'package:myxlo/blocs/drawer_bloc.dart';
+import 'package:myxlo/blocs/home_bloc.dart';
 //import 'package:myxlo/api/api_postalcode.dart';
 import 'package:myxlo/common/cep_field.dart';
 import 'package:myxlo/common/custom_drawer/custom_drawer.dart';
@@ -44,137 +45,158 @@ class _CreateScreenState extends State<CreateScreen> {
       drawer: CustomDrawer(),
       body: Form(
         key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            ImagesField(
-              onSaved: (images) {
-                ad.images = images;
-              },
-              initialValue: [],
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Título *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.blueGrey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              validator: (text) {
-                if (text.isEmpty) {
-                  return 'Campo obrigatório!';
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (t) {
-                ad.title = t;
-              },
-            ),
-            TextFormField(
-              maxLines: null,
-              decoration: InputDecoration(
-                labelText: 'Descrição *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.blueGrey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              validator: (text) {
-                if (text.trim().isEmpty) {
-                  return 'Campo obrigatório!';
-                } else if (text.trim().length < 10) {
-                  return 'Descrição muito curta!';
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (d) {
-                ad.description = d;
-              },
-            ),
-            CepField(
-              decoration: InputDecoration(
-                labelText: 'CEP *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.blueGrey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              onSaved: (a) {
-                ad.address = a;
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Preço *',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.blueGrey,
-                  fontSize: 18,
-                ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-              ),
-              keyboardType: const TextInputType.numberWithOptions(
-                decimal: true,
-                signed: false,
-              ),
-              inputFormatters: [
-                WhitelistingTextInputFormatter.digitsOnly,
-                RealInputFormatter(centavos: true),
-              ],
-              validator: (text) {
-                if (text.isEmpty) {
-                  return 'Campo obrigatório!';
-                } else if (int.tryParse(getSanitizedText(text)) == null) {
-                  return 'Utilize valores válidos!';
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (p) {
-                ad.price = int.parse(getSanitizedText(p)) / 100;
-              },
-            ),
-            HidePhoneWidget(
-              onSaved: (h) {
-                ad.hidePhone = h;
-              },
-              initialValue: false,
-            ),
-            Container(
-              height: 50,
-              child: RaisedButton(
-                color: Colors.green,
-                child: Text(
-                  'Enviar',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+        child: StreamBuilder<CreateState>(
+            stream: _createBloc.outState,
+            builder: (context, snapshot) {
+              if (snapshot.data == CreateState.LOADING) {
+                return Center(
+                  child: Container(
+                    width: 300,
+                    height: 300,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Colors.blueGrey,
+                      ),
+                      strokeWidth: 5,
+                    ),
                   ),
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
+                );
+              }
+              return ListView(
+                children: <Widget>[
+                  ImagesField(
+                    onSaved: (images) {
+                      ad.images = images;
+                    },
+                    initialValue: [],
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Título *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.blueGrey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    validator: (text) {
+                      if (text.isEmpty) {
+                        return 'Campo obrigatório!';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (t) {
+                      ad.title = t;
+                    },
+                  ),
+                  TextFormField(
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      labelText: 'Descrição *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.blueGrey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    validator: (text) {
+                      if (text.trim().isEmpty) {
+                        return 'Campo obrigatório!';
+                      } else if (text.trim().length < 10) {
+                        return 'Descrição muito curta!';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (d) {
+                      ad.description = d;
+                    },
+                  ),
+                  CepField(
+                    decoration: InputDecoration(
+                      labelText: 'CEP *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.blueGrey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    onSaved: (a) {
+                      ad.address = a;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Preço *',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.blueGrey,
+                        fontSize: 18,
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true,
+                      signed: false,
+                    ),
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly,
+                      RealInputFormatter(centavos: true),
+                    ],
+                    validator: (text) {
+                      if (text.isEmpty) {
+                        return 'Campo obrigatório!';
+                      } else if (int.tryParse(getSanitizedText(text)) == null) {
+                        return 'Utilize valores válidos!';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (p) {
+                      ad.price = int.parse(getSanitizedText(p)) / 100;
+                    },
+                  ),
+                  HidePhoneWidget(
+                    onSaved: (h) {
+                      ad.hidePhone = h;
+                    },
+                    initialValue: false,
+                  ),
+                  Container(
+                    height: 50,
+                    child: RaisedButton(
+                      color: Colors.green,
+                      child: Text(
+                        'Enviar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
 
-                    final bool success = await _createBloc.saveAd(ad);
+                          Provider.of<HomeBloc>(context).addAd(ad);
 
-                    if (success) {
-                      Provider.of<DrawerBloc>(context).setPage(0);
-                    }
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+                          final bool success = await _createBloc.saveAd(ad);
+
+                          if (success) {
+                            Provider.of<DrawerBloc>(context).setPage(0);
+                          }
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myxlo/blocs/home_bloc.dart';
 import 'package:myxlo/common/custom_drawer/custom_drawer.dart';
+import 'package:myxlo/models/ad.dart';
 import 'package:myxlo/screens/home/widgets/search_dialog.dart';
 import 'package:myxlo/screens/home/widgets/top_bar.dart';
 import 'package:provider/provider.dart';
@@ -86,6 +87,21 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: <Widget>[
           TopBar(),
+          Expanded(
+            child: StreamBuilder<List<Ad>>(
+              stream: _homeBloc.outAd,
+              builder: (context, snapshot) {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data[index].title),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
